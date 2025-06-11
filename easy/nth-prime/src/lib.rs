f42a6b035d216d255b6bb71b2c43f2540d3308a2
DevShiba <migerushiba@gmail.com>
2025-06-10 23:29:40 -0300

    feat(exercise): solving the nth prime exercise

---
+++ easy/nth-prime/src/lib.rs
@@ -0,0 +1,31 @@
+pub fn nth(n: u32) -> u32 {
+    if n == 0 {
+        return 2;
+    }
+    
+    let mut primes = vec![2];
+    let mut candidate = 3;
+    
+    while primes.len() <= n as usize {
+        let mut is_prime = true;
+        let sqrt_candidate = (candidate as f64).sqrt() as u32;
+        
+        for &prime in &primes {
+            if prime > sqrt_candidate {
+                break;
+            }
+            if candidate % prime == 0 {
+                is_prime = false;
+                break;
+            }
+        }
+        
+        if is_prime {
+            primes.push(candidate);
+        }
+        
+        candidate += 2; 
+    }
+    
+    primes[n as usize]
+}
