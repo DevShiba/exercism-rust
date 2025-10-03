8b1016d05ebae7546a17d83292cbf1ecc46ea4c8
devshiba <migerushiba@gmail.com>
2025-10-02 22:17:57 -0300

    feat(exercise): solve space age exercise

---
+++ medium/space-age/src/lib.rs
@@ -0,0 +1,40 @@
+const EARTH_YEAR_IN_SECONDS: f64 = 31557600.0;
+
+#[derive(Debug)]
+pub struct Duration {
+    seconds: u64,
+}
+
+impl From<u64> for Duration {
+    fn from(s: u64) -> Self {
+        Self { seconds: s }
+    }
+}
+
+pub trait Planet {
+    fn orbital_period() -> f64;
+
+    fn years_during(d: &Duration) -> f64 {
+        d.seconds as f64 / EARTH_YEAR_IN_SECONDS / Self::orbital_period()
+    }
+}
+
+macro_rules! planet {
+    ($name:ident, $period:expr) => {
+        pub struct $name;
+        impl Planet for $name {
+            fn orbital_period() -> f64 {
+                $period
+            }
+        }
+    };
+}
+
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
