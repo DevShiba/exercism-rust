4d66f481887383b5cba0971af53f055f4aff0f47
devshiba <migerushiba@gmail.com>
2025-10-11 22:37:13 -0300

    feat(exercise): solve new exercise paasio

---
+++ medium/paasio/src/lib.rs
@@ -0,0 +1,87 @@
+use std::io::{Read, Result, Write};
+
+pub struct ReadStats<R> {
+    wrapped: R,
+    reads: usize,
+    bytes_through: usize,
+}
+
+impl<R: Read> ReadStats<R> {
+    pub fn new(wrapped: R) -> ReadStats<R> {
+        ReadStats {
+            wrapped,
+            reads: 0,
+            bytes_through: 0,
+        }
+    }
+
+    pub fn get_ref(&self) -> &R {
+        &self.wrapped
+    }
+
+    pub fn bytes_through(&self) -> usize {
+        self.bytes_through
+    }
+
+    pub fn reads(&self) -> usize {
+        self.reads
+    }
+}
+
+impl<R: Read> Read for ReadStats<R> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.reads += 1;
+        match self.wrapped.read(buf) {
+            Ok(bytes) => {
+                self.bytes_through += bytes;
+                Ok(bytes)
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
+
+pub struct WriteStats<W> {
+    wrapped: W,
+    writes: usize,
+    bytes_through: usize,
+}
+
+impl<W: Write> WriteStats<W> {
+    pub fn new(wrapped: W) -> WriteStats<W> {
+        WriteStats {
+            wrapped,
+            writes: 0,
+            bytes_through: 0,
+        }
+    }
+
+    pub fn get_ref(&self) -> &W {
+        &self.wrapped
+    }
+
+    pub fn bytes_through(&self) -> usize {
+        self.bytes_through
+    }
+
+    pub fn writes(&self) -> usize {
+        self.writes
+    }
+}
+
+impl<W: Write> Write for WriteStats<W> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        self.writes += 1;
+        match self.wrapped.write(buf) {
+            Ok(bytes) => {
+                self.bytes_through += bytes;
+                Ok(bytes)
+            }
+            Err(e) => Err(e),
+        }
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.wrapped.flush()
+    }
+}
